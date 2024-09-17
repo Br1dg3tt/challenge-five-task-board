142ed08272ca401cc5062778d902a969397dab81
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-09-17 13:25:49 -0600

    creating card attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,49 @@ function createTaskCard(task) {
         } else {
           taskDeadlineColor = "on-track";
         }
+//creates a div class for card placement
+  const cardBox = $("<div>");
+  //adding jquery and boostrap css classes
+  cardBox.addClass(
+    "card draggable mb-2 task-card " + taskDeadlineColor.toString()
+  );
+  //adding a unique id attribute
+  cardBox.attr("data-task-id", task.id);
+
+  //creates div in body
+  const cardBody = $("<div>");
+  cardBody.addClass("card-body");
+
+  //
+  const cardTitle = $("<h5>");
+  cardTitle.addClass("card-title");
+  cardTitle.text(task.title);
+
+  //
+  const cardText = $("<p>");
+  cardText.addClass("card-text");
+  cardText.text(task.description);
+
+  //
+  const cardDeadline = $("<p>");
+  cardDeadline.addClass("card-text text-muted");
+  cardDeadline.text(deadline.format("MM-DD-YYYY"));
+
+  //
+  const cardDeleteBtn = $("<button>");
+  cardDeleteBtn.addClass("btn btn-sm btn-danger float-end delete-task");
+  cardDeleteBtn.attr("data-task-id", task.id);
+  cardDeleteBtn.text("Delete");
+
+  //
+  cardBody.append(cardTitle);
+  cardBody.append(cardText);
+  cardBody.append(cardDeadline);
+  cardBody.append(cardDeleteBtn);
+  cardBox.append(cardBody);
+
+  return cardBox;
+}
 }
 
 // Todo: create a function to render the task list and make cards draggable
